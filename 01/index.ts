f601279befa76a650055c968a400f36da6605bb4
Hereward Mills <herewardnjmills@gmail.com>
2022-12-01 21:10:43 +0000

    convert to typescript

--- 01/index.ts
+++ 01/index.ts
@@ -2,14 +2,14 @@ const fs = require("fs");
 const { EOL } = require("os");
 
 try {
-    data = fs.readFileSync("input", "utf8");
+    var data = fs.readFileSync("input", "utf8");
     data = data.split(EOL);
   } catch (err) {
     console.error(err);
   }
 
 var index = 0;
-var munged = data.reduce((a, c) => {
+var munged = data.reduce((a: number[], c: string) => {
     if (c.length == 0){
         index = index + 1
         a[index] = 0
@@ -22,7 +22,6 @@ var munged = data.reduce((a, c) => {
 }, [0])
 
 munged.sort().reverse()
-console.log(munged)
 
 const answer = munged[0] + munged[1] + munged[2]
 
---
+++ 02/index.ts
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const { EOL } = require("os");
+
+try {
+  var data = fs.readFileSync("input", "utf8");
+  data = data.split(EOL);
+} catch (err) {
+  console.error(err);
+}
+
+var index = 0;
+var munged = data.reduce(
+  (a: number[], c: string) => {
+    if (c.length == 0) {
+      index = index + 1;
+      a[index] = 0;
+      return a;
+    }
+
+    a[index] = a[index] + parseInt(c);
+    return a;
+  },
+  [0]
+);
+
+munged.sort().reverse();
+
+const answer = munged[0] + munged[1] + munged[2];
+
+console.log(answer);
+
+export {};
