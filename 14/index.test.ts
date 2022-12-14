a2b0e21b1122fee6abc27608403db3de941ea1f1
Hereward Mills <herewardnjmills@gmail.com>
2022-12-14 05:49:13 +0000

    14a

---
+++ 14/index.test.ts
@@ -0,0 +1,122 @@
+import each from "jest-each";
+import { parseToPacket, comparePackets, sortPackets } from ".";
+
+describe("parseToInstruction", () => {
+  each([["[1,1,3,1,1]", [1, 1, 3, 1, 1]]]).test(
+    "for %s, returns %s",
+    (string, expected) => {
+      expect(parseToPacket(string)).toStrictEqual(expected);
+    }
+  );
+});
+
+describe("comparePackets", () => {
+  each([
+    [1, 1, undefined],
+    [[], [], undefined],
+    [[], [[]], true],
+    [1, 9, true],
+    [9, 1, false],
+    [1, [9], true],
+    [[1], 9, true],
+    [[], 9, true],
+    [[1, 1, 3, 1, 1], [1, 1, 5, 1, 1], true],
+    [[1, 1, 5, 1, 1], [1, 1, 3, 1, 1], false],
+    [[1, 1, 5, 1, 1], [1, 1, 5, 1, 1], undefined],
+    [[1, 1, 5], [1, 1, 3, 1, 1], false],
+    [[[1], [2, 3, 4]], [[1], 4], true],
+    [[9], [[8, 7, 6]], false],
+    [[[4, 4], 4, 4], [[4, 4], 4, 4, 4], true],
+    [[7, 7, 7, 7], [7, 7, 7], false],
+    [[], [3], true],
+    [[[[]]], [[]], false],
+    [[0, 0, 0], 2, true],
+    [
+      [1, [2, [3, [4, [5, 6, 7]]]], 8, 9],
+      [1, [2, [3, [4, [5, 6, 0]]]], 8, 9],
+      false,
+    ],
+  ]).test("for %s and %s, returns %s", (left, right, expected) => {
+    expect(comparePackets(left, right)).toStrictEqual(expected);
+  });
+});
+
+describe("sortpackets", () => {
+  each([
+    [1, 1, 0],
+    [[], [], 0],
+    [[], [[]], 1],
+    [1, 9, 1],
+    [9, 1, -1],
+    [1, [9], 1],
+    [[1], 9, 1],
+    [[], 9, 1],
+    [[1, 1, 3, 1, 1], [1, 1, 5, 1, 1], 1],
+    [[1, 1, 5, 1, 1], [1, 1, 3, 1, 1], -1],
+    [[1, 1, 5, 1, 1], [1, 1, 5, 1, 1], 0],
+    [[1, 1, 5], [1, 1, 3, 1, 1], -1],
+    [[[1], [2, 3, 4]], [[1], 4], 1],
+    [[9], [[8, 7, 6]], -1],
+    [[[4, 4], 4, 4], [[4, 4], 4, 4, 4], 1],
+    [[7, 7, 7, 7], [7, 7, 7], -1],
+    [[], [3], 1],
+    [[[[]]], [[]], -1],
+    [[0, 0, 0], 2, 1],
+    [
+      [1, [2, [3, [4, [5, 6, 7]]]], 8, 9],
+      [1, [2, [3, [4, [5, 6, 0]]]], 8, 9],
+      -1,
+    ],
+  ]).test("for %s and %s, returns %s", (left, right, expected) => {
+    expect(sortPackets(left, right)).toStrictEqual(expected);
+  });
+});
+
+describe("comparePackets - actual inputs", () => {
+  each([
+    [
+      [
+        [],
+        [[0, [10, 1], 3, [4]], 9, [2, 10, 4, 10]],
+        [5, [0, 0, [1, 2, 7, 5]]],
+      ],
+      [
+        [
+          [],
+          [
+            [8, 5, 2, 8, 0],
+            [4, 7, 0, 5],
+            [2, 7, 7],
+          ],
+          4,
+        ],
+        [4],
+        [2, 8, [[9, 4, 8]], 6, 10],
+        [[[3, 0, 2, 5], [], 4, [7, 9], [1, 5, 10, 9, 6]], 4],
+        [],
+      ],
+      true,
+    ],
+    [
+      [[[8], 0, [[3], 10]], []],
+      [
+        [5, 1, [[0, 10, 5], 3, 10], 9],
+        [[5, [0, 9, 3], [2, 1, 5], 10]],
+        [1, [[10, 1, 9, 10], [10], 2, 1], [[9, 0, 8, 6, 7], 1], [3]],
+      ],
+      false,
+    ],
+    [[3, 8, 3, 3], [3, 8, 3, 3, 4], true],
+  ]).test("for %s and %s, returns %s", (left, right, expected) => {
+    expect(comparePackets(left, right)).toStrictEqual(expected);
+  });
+});
+
+// describe("comparePackets", () => {
+//   each([[[[1], [2, 3, 4]], [[1], 4], true]]).test(
+//     "for %s and %s, returns %s",
+//     (left, right, expected) => {
+//       expect(comparePackets(left, right)).toStrictEqual(expected);
+//     }
+//   );
+// });
---
+++ 14/index.ts
@@ -0,0 +1,101 @@
+import _, { add } from "lodash";
+import { readFile } from "../common";
+
+var data = readFile("input");
+
+var lines = data.map((line) => {
+  return line
+    .split(" -> ")
+    .map((point) => point.split(",").map((c) => parseInt(c)));
+});
+
+function getVectorFromAtoB(pointA: number[], pointB: number[]) {
+  return [pointB[0] - pointA[0], pointB[1] - pointA[1]];
+}
+
+function addVector(pointA: number[], pointB: number[]) {
+  return [pointB[0] + pointA[0], pointB[1] + pointA[1]];
+}
+
+function normaliseVector(vector: number[]) {
+  const size = Math.max(...vector.map((v) => Math.abs(v)));
+  return vector.map((v) => v / size);
+}
+
+const points = lines.flatMap((line) => {
+  var points = [];
+  for (var i = 0; i < line.length - 1; i++) {
+    var diff = getVectorFromAtoB(line[i], line[i + 1]);
+    diff = normaliseVector(diff);
+    var point = _.clone(line[i]);
+    while (!_.isEqual(point, line[i + 1])) {
+      points.push(point);
+      point = addVector(point, diff);
+    }
+    points.push(point);
+  }
+  return points;
+});
+
+function pointToString(point: number[]) {
+  return point.join("|");
+}
+
+const blockedPoints: any = {};
+const maxDepth = points.reduce((a, c) => {
+  return Math.max(a, c[1]);
+}, 0);
+
+points.forEach((point) => (blockedPoints[pointToString(point)] = "rock"));
+
+const sandVectors = [
+  [0, 1],
+  [-1, 1],
+  [1, 1],
+];
+
+function isBlocked(point: number[]) {
+  const item = blockedPoints[pointToString(point)];
+  return item != undefined;
+}
+
+function getNewSand(sand: number[]) {
+  var newSand: number[] = [];
+  for (var i = 0; i < sandVectors.length; i++) {
+    var newSand = addVector(sand, sandVectors[i]);
+    if (!isBlocked(newSand)) {
+      return newSand;
+    }
+  }
+  return sand;
+}
+
+function fallingForever(point: number[]) {
+  return point[1] > maxDepth;
+}
+
+var isFallingForever = false;
+
+var sandCount = 0;
+while (!isFallingForever) {
+  var sand = [500, 0];
+  sandCount++;
+  var newSand = _.clone(sand);
+  var isStopped = false;
+
+  while (!isStopped) {
+    // console.log(newSand);
+    newSand = getNewSand(sand);
+    isStopped = _.isEqual(sand, newSand);
+    sand = newSand;
+    isFallingForever = fallingForever(newSand);
+    if (isFallingForever) {
+      break;
+    }
+  }
+  blockedPoints[pointToString(newSand)] = "sand";
+}
+
+console.log(sandCount - 1);
+
+export {};
