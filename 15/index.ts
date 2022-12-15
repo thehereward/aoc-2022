e9d74b89aa5354a0cd19397977efb5a7d11022a7
Hereward Mills <herewardnjmills@gmail.com>
2022-12-15 06:17:53 +0000

    15b

--- 15/index.ts
+++ 15/index.ts
@@ -1,6 +1,8 @@
 import _ from "lodash";
 import { readFile } from "../common";
 
+const start = Date.now();
+
 var data = readFile("input");
 
 const regex =
@@ -28,9 +30,8 @@ function distance(a: number[], b: number[]) {
   return Math.abs(b[0] - a[0]) + Math.abs(b[1] - a[1]);
 }
 
-const row = 2000000;
-
-const maxMin: any[] = [];
+//const row = 2000000;
+// const row = 10;
 
 export function getHoizontalDistance(
   targetRow: number,
@@ -43,35 +44,81 @@ export function getHoizontalDistance(
   return horizontalDistance;
 }
 
-lines.forEach((line) => {
-  const horizontalDistance = getHoizontalDistance(
-    row,
-    line.sensor,
-    line.distance
-  );
-  if (horizontalDistance >= 0) {
-    const max = line.sensor[0] + horizontalDistance;
-    const min = line.sensor[0] - horizontalDistance;
-    maxMin.push({
-      max,
-      min,
-    });
+const rowMax = 4000000;
+const colMax = 4000000;
+
+const allMaxMin: any[][] = [];
+for (var row = 0; row <= rowMax; row++) {
+  const maxMin: any[] = [];
+  lines.forEach((line) => {
+    const horizontalDistance = getHoizontalDistance(
+      row,
+      line.sensor,
+      line.distance
+    );
+    if (horizontalDistance >= 0) {
+      const max = line.sensor[0] + horizontalDistance;
+      const min = line.sensor[0] - horizontalDistance;
+      maxMin.push({
+        max,
+        min,
+      });
+    }
+  });
+  allMaxMin.push(maxMin);
+}
+
+function isInRange(range: any, value: number) {
+  return value >= range.min && value <= range.max;
+}
+
+const allValidPoints = [];
+for (var row = 0; row < rowMax; row++) {
+  const validPoints = [];
+  for (var col = 0; col < colMax; col++) {
+    const exclusions = allMaxMin[row];
+    const inRange = exclusions.reduce((a, c) => {
+      if (isInRange(c, col)) {
+        a = true;
+        col = c.max;
+      }
+      return a;
+    }, false);
+    if (!inRange) {
+      validPoints.push(col);
+    } else {
+    }
   }
+  allValidPoints.push(validPoints);
+}
+
+// const allResult = allMaxMin.map((maxMin) => {
+//   return maxMin.reduce(
+//     (a, b) => {
+//       if (b.max > a.max) {
+//         a.max = b.max;
+//       }
+//       if (b.min < a.min) {
+//         a.min = b.min;
+//       }
+//       return a;
+//     },
+//     { max: -Infinity, min: Infinity }
+//   );
+// });
+
+// const result = allResult[10];
+// console.log(`Part 1: ${result.max - result.min}`);
+
+allValidPoints.forEach((points, i) => {
+  points.forEach((point) => {
+    console.log(
+      `Part 2: x: ${point}, y: ${i}, strength: ${point * 4000000 + i}`
+    );
+  });
 });
 
-const result = maxMin.reduce(
-  (a, b) => {
-    if (b.max > a.max) {
-      a.max = b.max;
-    }
-    if (b.min < a.min) {
-      a.min = b.min;
-    }
-    return a;
-  },
-  { max: -Infinity, min: Infinity }
-);
-
-console.log(result.max - result.min);
+const end = Date.now();
+console.log(end - start);
 
 export {};
