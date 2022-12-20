dff27aeb3dd151cc9f32c4841e7c2d8d90b3cf27
Hereward Mills <herewardnjmills@gmail.com>
2022-12-20 05:54:07 +0000

    20a

--- 20/index.test.ts
+++ 20/index.test.ts
@@ -1,117 +1,147 @@
 import each from "jest-each";
-import { instructionToBluePrint, buildBlueprint, part1, part2 } from ".";
+import { moveItem } from ".";
 
-describe("parses input lines to blue prints", () => {
+describe("move item", () => {
+  const list = [
+    { value: 1 },
+    { value: 2 },
+    { value: -3 },
+    { value: 3 },
+    { value: -2 },
+    { value: 0 },
+    { value: 4 },
+  ];
   each([
     [
-      "Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.",
-      {
-        Id: 1,
-        OreRobotCost: [4, 0, 0],
-        ClayRobotCost: [2, 0, 0],
-        ObsidianRobotCost: [3, 14, 0],
-        GeodeRobotCost: [2, 0, 7],
-      },
+      list,
+      list[0],
+      [
+        { value: 2 },
+        { value: 1 },
+        { value: -3 },
+        { value: 3 },
+        { value: -2 },
+        { value: 0 },
+        { value: 4 },
+      ],
     ],
-    [
-      "Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.",
-      {
-        Id: 2,
-        OreRobotCost: [2, 0, 0],
-        ClayRobotCost: [3, 0, 0],
-        ObsidianRobotCost: [3, 8, 0],
-        GeodeRobotCost: [3, 0, 12],
-      },
-    ],
-    ,
-  ]).test("for (%s) returns %s", (input, expected) => {
-    expect(instructionToBluePrint(input)).toStrictEqual(expected);
+  ]).test("processes index 0 (2)", (list, item, expected) => {
+    expect(moveItem(list, item)).toEqual(expected);
   });
 });
 
-describe("gets all possible builds", () => {
+describe("move item", () => {
+  const list = [
+    { value: 1 },
+    { value: 2 },
+    { value: -2 },
+    { value: -3 },
+    { value: 0 },
+    { value: 3 },
+    { value: 4 },
+  ];
   each([
     [
-      {
-        Id: 1,
-        OreRobotCost: [4, 0, 0],
-        ClayRobotCost: [2, 0, 0],
-        ObsidianRobotCost: [3, 14, 0],
-        GeodeRobotCost: [2, 0, 7],
-      },
-      {
-        Minute: 0,
-        Ore: { Quantity: 2, Robots: 1, NewRobots: 0 },
-        Clay: { Quantity: 0, Robots: 0, NewRobots: 0 },
-        Obsidian: { Quantity: 0, Robots: 0, NewRobots: 0 },
-        Geode: { Quantity: 0, Robots: 0, NewRobots: 0 },
-      },
+      list,
+      list[2],
       [
-        {
-          Minute: 0,
-          Ore: { Quantity: 2, Robots: 1, NewRobots: 0 },
-          Clay: { Quantity: 0, Robots: 0, NewRobots: 0 },
-          Obsidian: { Quantity: 0, Robots: 0, NewRobots: 0 },
-          Geode: { Quantity: 0, Robots: 0, NewRobots: 0 },
-        },
-        {
-          Minute: 0,
-          Ore: { Quantity: 0, Robots: 1, NewRobots: 0 },
-          Clay: { Quantity: 0, Robots: 0, NewRobots: 1 },
-          Obsidian: { Quantity: 0, Robots: 0, NewRobots: 0 },
-          Geode: { Quantity: 0, Robots: 0, NewRobots: 0 },
-        },
+        { value: 1 },
+        { value: 2 },
+        { value: -3 },
+        { value: 0 },
+        { value: 3 },
+        { value: 4 },
+        { value: -2 },
       ],
     ],
-    [
-      {
-        Id: 1,
-        OreRobotCost: [4, 0, 0],
-        ClayRobotCost: [2, 0, 0],
-        ObsidianRobotCost: [3, 14, 0],
-        GeodeRobotCost: [2, 0, 7],
-      },
-      {
-        Minute: 0,
-        Ore: { Quantity: 4, Robots: 1, NewRobots: 0 },
-        Clay: { Quantity: 0, Robots: 0, NewRobots: 0 },
-        Obsidian: { Quantity: 0, Robots: 0, NewRobots: 0 },
-        Geode: { Quantity: 0, Robots: 0, NewRobots: 0 },
-      },
-      [
-        {
-          Minute: 0,
-          Ore: { Quantity: 4, Robots: 1, NewRobots: 0 },
-          Clay: { Quantity: 0, Robots: 0, NewRobots: 0 },
-          Obsidian: { Quantity: 0, Robots: 0, NewRobots: 0 },
-          Geode: { Quantity: 0, Robots: 0, NewRobots: 0 },
-        },
-        {
-          Minute: 0,
-          Ore: { Quantity: 0, Robots: 1, NewRobots: 1 },
-          Clay: { Quantity: 0, Robots: 0, NewRobots: 0 },
-          Obsidian: { Quantity: 0, Robots: 0, NewRobots: 0 },
-          Geode: { Quantity: 0, Robots: 0, NewRobots: 0 },
-        },
-        {
-          Minute: 0,
-          Ore: { Quantity: 2, Robots: 1, NewRobots: 0 },
-          Clay: { Quantity: 0, Robots: 0, NewRobots: 1 },
-          Obsidian: { Quantity: 0, Robots: 0, NewRobots: 0 },
-          Geode: { Quantity: 0, Robots: 0, NewRobots: 0 },
-        },
-      ],
-    ],
-  ]).test("for (%s) and (%s) returns %s", (blueprint, state, expected) => {
-    expect(buildBlueprint(blueprint, state)).toStrictEqual(expected);
+  ]).test("processes index 2 (-2)", (list, item, expected) => {
+    expect(moveItem(list, item)).toEqual(expected);
   });
 });
 
-describe("it passes", () => {
-  it("part 1", () => {
-    expect(part1()).toBe(1092);
+describe("move item", () => {
+  const list = [
+    { value: 1 },
+    { value: 2 },
+    { value: -3 },
+    { value: 0 },
+    { value: 3 },
+    { value: 4 },
+    { value: -2 },
+  ];
+  each([
+    [
+      list,
+      list[5],
+      [
+        { value: 1 },
+        { value: 2 },
+        { value: -3 },
+        { value: 4 },
+        { value: 0 },
+        { value: 3 },
+        { value: -2 },
+      ],
+    ],
+  ]).test("processes index 5 (4)", (list, item, expected) => {
+    expect(moveItem(list, item)).toEqual(expected);
   });
-  it("part 2", () => {
-    expect(part2()).toBe(3542);
+});
+
+describe("move item", () => {
+  const list = [
+    { value: 1 },
+    { value: 2 },
+    { value: -3 },
+    { value: 0 },
+    { value: 3 },
+    { value: 4 },
+    { value: -2 },
+  ];
+  each([
+    [
+      list,
+      list[3],
+      [
+        { value: 1 },
+        { value: 2 },
+        { value: -3 },
+        { value: 0 },
+        { value: 3 },
+        { value: 4 },
+        { value: -2 },
+      ],
+    ],
+  ]).test("processes index 3 (0)", (list, item, expected) => {
+    expect(moveItem(list, item)).toEqual(expected);
+  });
+});
+
+describe("move item", () => {
+  const list = [
+    { value: 4 },
+    { value: -2 },
+    { value: 5 },
+    { value: 6 },
+    { value: 7 },
+    { value: 8 },
+    { value: 9 },
+  ];
+  each([
+    [
+      list,
+      list[1],
+      [
+        { value: 4 },
+        { value: 5 },
+        { value: 6 },
+        { value: 7 },
+        { value: 8 },
+        { value: -2 },
+        { value: 9 },
+      ],
+    ],
+  ]).test("processes index 1 (-2)", (list, item, expected) => {
+    expect(moveItem(list, item)).toEqual(expected);
   });
 });
--- 20/index.ts
+++ 20/index.ts
@@ -1,213 +1,75 @@
-import _ from "lodash";
+import _, { indexOf } from "lodash";
 import { readFile, getTimeLogger } from "../common";
 
 const logTime = getTimeLogger();
 
 var data = readFile("input");
 
-const blueprintRegex =
-  /^Blueprint (\d+): Each ore robot costs (\d+) ore. Each clay robot costs (\d+) ore. Each obsidian robot costs (\d+) ore and (\d+) clay. Each geode robot costs (\d+) ore and (\d+) obsidian.$/;
-
-class ResourceState {
-  Quantity: number;
-  Robots: number;
-  NewRobots: number;
+class Item {
+  value: number;
 }
 
-class State {
-  Minute: number;
-  Ore: ResourceState;
-  Clay: ResourceState;
-  Obsidian: ResourceState;
-  Geode: ResourceState;
-}
-
-const initialState: State = {
-  Minute: 0,
-  Ore: { Quantity: 0, Robots: 1, NewRobots: 0 },
-  Clay: { Quantity: 0, Robots: 0, NewRobots: 0 },
-  Obsidian: { Quantity: 0, Robots: 0, NewRobots: 0 },
-  Geode: { Quantity: 0, Robots: 0, NewRobots: 0 },
-};
-
-// [Ore, Clay, Obsidian]
-class Blueprint {
-  Id: number;
-  OreRobotCost: number[];
-  ClayRobotCost: number[];
-  ObsidianRobotCost: number[];
-  GeodeRobotCost: number[];
-}
-
-export function instructionToBluePrint(line: string): Blueprint {
-  const match = line.match(blueprintRegex);
+var items: Item[] = data.map((line) => {
   return {
-    Id: parseInt(match[1]),
-    OreRobotCost: [parseInt(match[2]), 0, 0],
-    ClayRobotCost: [parseInt(match[3]), 0, 0],
-    ObsidianRobotCost: [parseInt(match[4]), parseInt(match[5]), 0],
-    GeodeRobotCost: [parseInt(match[6]), 0, parseInt(match[7])],
+    value: parseInt(line),
   };
-}
-
-const blueprints: Blueprint[] = data.map((line) => {
-  return instructionToBluePrint(line);
 });
 
-function gatherResources(state: State): State {
-  // var newState = _.cloneDeep(state);
-  var newState = state;
-  newState.Ore.Quantity = state.Ore.Quantity + state.Ore.Robots;
-  newState.Clay.Quantity = state.Clay.Quantity + state.Clay.Robots;
-  newState.Obsidian.Quantity = state.Obsidian.Quantity + state.Obsidian.Robots;
-  newState.Geode.Quantity = state.Geode.Quantity + state.Geode.Robots;
-  return newState;
+// console.log(items.length);
+// var set = new Set(items);
+// console.log(set.size);
+
+var newOrder = _.clone(items);
+
+function printList(list: Item[]) {
+  logTime(list.map((i) => i.value).join(", "));
 }
 
-function addRobots(state: State): State {
-  // var newState = _.cloneDeep(state);
-  var newState = state;
-  newState.Ore.Robots = state.Ore.NewRobots + state.Ore.Robots;
-  newState.Ore.NewRobots = 0;
-  newState.Clay.Robots = state.Clay.NewRobots + state.Clay.Robots;
-  newState.Clay.NewRobots = 0;
-  newState.Obsidian.Robots = state.Obsidian.NewRobots + state.Obsidian.Robots;
-  newState.Obsidian.NewRobots = 0;
-  newState.Geode.Robots = state.Geode.NewRobots + state.Geode.Robots;
-  newState.Geode.NewRobots = 0;
-  return newState;
-}
-
-function advanceTime(state: State): State {
-  // var newState = _.cloneDeep(state);
-  var newState = state;
-  newState.Minute = state.Minute + 1;
-  return newState;
-}
-
-function resourceToString(resource: ResourceState): string {
-  return `${resource.Quantity} (${resource.Robots})`;
-}
-
-function printState(state: State) {
-  console.log(
-    `Time: ${state.Minute} | Ore: ${resourceToString(
-      state.Ore
-    )} | Clay: ${resourceToString(state.Clay)} | Obsidian: ${resourceToString(
-      state.Obsidian
-    )} | Geode: ${resourceToString(state.Geode)} `
-  );
-}
-
-function canBuildRobot(cost: number[], state: State): boolean {
-  return (
-    cost[0] <= state.Ore.Quantity &&
-    cost[1] <= state.Clay.Quantity &&
-    cost[2] <= state.Obsidian.Quantity
-  );
-}
-
-function buildRobot(cost: number[], state: State): State {
-  var newState = _.cloneDeep(state);
-  newState.Ore.Quantity = newState.Ore.Quantity - cost[0];
-  newState.Clay.Quantity = newState.Clay.Quantity - cost[1];
-  newState.Obsidian.Quantity = newState.Obsidian.Quantity - cost[2];
-  return newState;
-}
-
-export function buildBlueprint(blueprint: Blueprint, state: State): State[] {
-  const newStates: State[] = [_.cloneDeep(state)];
-  const canBuildOre = canBuildRobot(blueprint.OreRobotCost, state);
-  if (canBuildOre) {
-    var newState = buildRobot(blueprint.OreRobotCost, state);
-    newState.Ore.NewRobots = newState.Ore.NewRobots + 1;
-    newStates.push(newState);
+export function moveItem(list: Item[], item: Item) {
+  // printList(list);
+  var index = list.indexOf(item);
+  list.splice(index, 1);
+  var length = list.length;
+  var value = item.value;
+  var newIndex = index + value;
+  if (newIndex > length) {
+    newIndex = newIndex % length;
+  } else if (newIndex < 0) {
+    // while (newIndex < 0) {
+    //   newIndex = newIndex + length;
+    // }
+    newIndex = newIndex % length;
+  } else if (newIndex == 0) {
+    newIndex = length;
   }
 
-  const canBuildClay = canBuildRobot(blueprint.ClayRobotCost, state);
-  if (canBuildClay) {
-    var newState = buildRobot(blueprint.ClayRobotCost, state);
-    newState.Clay.NewRobots++;
-    newStates.push(newState);
-  }
+  // console.log({
+  //   index,
+  //   length,
+  //   value,
+  //   newIndex,
+  // });
 
-  const canBuildObsidian = canBuildRobot(blueprint.ObsidianRobotCost, state);
-  if (canBuildObsidian) {
-    var newState = buildRobot(blueprint.ObsidianRobotCost, state);
-    newState.Obsidian.NewRobots++;
-    newStates.push(newState);
-  }
-
-  const canBuildGeode = canBuildRobot(blueprint.GeodeRobotCost, state);
-  if (canBuildGeode) {
-    var newState = buildRobot(blueprint.GeodeRobotCost, state);
-    newState.Geode.NewRobots++;
-    newStates.push(newState);
-  }
-
-  return newStates;
+  list.splice(newIndex, 0, item);
+  return list;
 }
 
-function getResourceScore(state: ResourceState, weight: number): number {
-  return state.Quantity * weight + state.Robots * weight * 2;
-}
+items.forEach((item) => {
+  newOrder = moveItem(newOrder, item);
+});
+// printList(newOrder);
 
-function getScore(state: State): number {
-  return (
-    getResourceScore(state.Ore, 1) +
-    getResourceScore(state.Clay, 10) +
-    getResourceScore(state.Obsidian, 100) +
-    getResourceScore(state.Geode, 1000)
-  );
-}
+var indexOfZero = newOrder.findIndex((item) => item.value == 0);
 
-function getMaxGeodes(blueprints: Blueprint[], timeLimit: number) {
-  var maxGeodes: number[] = [];
-  blueprints.forEach((blueprint) => {
-    var states = [_.cloneDeep(initialState)];
-    for (var time = 0; time < timeLimit; time++) {
-      states = states
-        .flatMap((state) => buildBlueprint(blueprint, state))
-        .flatMap((state) => gatherResources(state))
-        .flatMap((state) => addRobots(state))
-        .flatMap((state) => advanceTime(state))
-        .sort((a, b) => getScore(b) - getScore(a))
-        .slice(0, 400);
-    }
+var coords = [1000, 2000, 3000].map((number) => {
+  var index = (indexOfZero + number) % newOrder.length;
+  return newOrder[index];
+});
 
-    states.sort((a, b) => b.Geode.Quantity - a.Geode.Quantity);
-    printState(states[0]);
-    maxGeodes.push(states[0].Geode.Quantity);
-  });
-  return maxGeodes;
-}
+console.log(coords.reduce((a, c) => a + c.value, 0));
 
-export function part1() {
-  console.log("Part 1");
-  var part1Time = getTimeLogger();
-  var maxGeodes = getMaxGeodes(blueprints, 24);
-  var totalQuality = maxGeodes.reduce((a, c, i) => {
-    return a + c * (i + 1);
-  });
-  console.log(`Part 1: ${totalQuality}`);
-  part1Time();
-  return totalQuality;
-}
-
-export function part2() {
-  console.log("Part 2");
-  var part2Time = getTimeLogger();
-  var maxGeodes = getMaxGeodes(blueprints.slice(0, 3), 32);
-  var product = maxGeodes.reduce((a, c) => {
-    return a * c;
-  });
-  console.log(`Part 2: ${product}`);
-  part2Time();
-  return product;
-}
-
-part1();
-part2();
+// Part 1
+// 12570 - incorrect
 
 logTime();
 export {};
